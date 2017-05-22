d409f1c43d9dd4bd0c935b1e7fe18d31daa3c9d3
Guillaume <tremeauguillaume@gmail.com>
2017-05-22 17:32:31 +0200

    questions 4 6 7

--- menu.sh
+++ menu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+LANGUAGE="FR"
+if [ $LANGUAGE == "US" ];then
+	MESSFIXREP="Wich repository ? ('..' parent repository)"
+	MESSCREERREP="Enter your new folder name"
+	MESSEDITTEXT="Enter the file name"
+fi
 afficheEspaceDisque()
 {
 	clear
@@ -9,16 +14,65 @@ afficheEspaceDisque()
 
 fixerRepertoire()
 {
+	USERREP="."
 	clear
 	ls
 	echo "Quel repertoire ? ('..' pour le parent)"
-	cd 
+	read USERREP
+	cd $USERREP
 }
-REPERTOIRE=`pwd`
+creerRepertoire()
+{
+	USERREP="defaultRep"
+	clear
+	echo "Entrez le nom de votre repertoire"
+	read USERREP
+	mkdir $USERREP
+}
+editerTexte()
+{
+	USERREP=""
+	clear
+	ls
+	echo "Entrez le nom du fichier que vous souhaitez modifier"
+	read USERREP
+	vim $USERREP
+}
+envoiMail()
+{
+	ADDRESS=""
+	echo "Entrez le destinataire (ex: user@hello.com) : "
+	read ADDRESS
+	SUBJECT=""
+	echo "Entrez l'objet du mail : "
+	read SUBJECT
+	BODY=""
+	echo "Entrez le contenu du mail : "
+	read BODY
+	echo $body | mail -s $SUBJECT $ADDRESS
+}
+envoiMailFichier()
+{
+	ADDRESS=""
+	echo "Entrez le destinataire (ex: user@hello.com) : "
+	read ADDRESS
+	SUBJECT=""
+	echo "Entrez l'objet du mail : "
+	read SUBJECT
+	BODY=""
+	echo "Entrez le contenu du mail : "
+	read BODY
+	NOMFIL=""
+	echo"Entrez le chemin du fichier à éditer : "
+	read $NOMFIL
+	echo $body | mutt -a $NOMFIL -s $SUBJECT -- $ADDRESS
+}
 USER="A"
 while [ $USER != "Q" ]; do
 	clear
-	./tp3.sh $REPERTOIRE
+	REPERTOIRE=`pwd`
+#repertoire du fichier source
+	~/Cours/SYSTEME/SYSTP3/tp3.sh $REPERTOIRE $LANGUAGE
 	read USER
 	case $USER in
 		"D")
@@ -28,16 +82,16 @@ while [ $USER != "Q" ]; do
 			fixerRepertoire
 			;;
 		"C")
-			
+			creerRepertoire
 			;;
 		"E")
-			
+			editerTexte
 			;;
 		"M")
-			
+			envoiMail
 			;;
 		"S")
-			
+			envoiMailFichier
 			;;
 		"Q")
 			
--- tp3.sh
+++ tp3.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+echo "$2"
+if [ $2 == "US" ];then
+	BONJOUR="Hello  "
+	MESSREP="Default repository   "
+	MESSD="Show disk usage                "
+	MESSF="Switch default folder         "
+	MESSC="Create Folder      "
+	MESSE="Edit a text    "
+	MESSM="Send a mail    "
+	MESSS="Send a mail with an attachment"
+	MESSQ="Disconnect    "
+	MESSCHOICE="Your Choice (D,F,C,E,M,S,Q) ?"
+fi
+if [ $2 == "FR" ];then
+	BONJOUR="Bonjour"
+	MESSREP="Répertoire par defaut"
+	MESSD="afficher l'espace disque occupé"
+	MESSF="fixer le répertoire par défaut"
+	MESSC="créer un répertoire"
+	MESSE="Editer un texte"
+	MESSM="Envoyer un mail"
+	MESSS="Envoyer un fichier par mail   "
+	MESSQ="Se Déconnecter"
+	MESSCHOICE="Votre Choix (D,F,C,E,M,S,Q) ?"
+fi
 
 echo "_____________________________________________________"
 PSEUDO=`cat ~/pseudo.cat`
@@ -9,7 +34,7 @@ while [ $NBESPACE != "0" ]; do
 	ESPACE="$ESPACE "
 	NBESPACE=`expr $NBESPACE "-" "1"`
 done
-echo "|        Bonjour : $PSEUDO$ESPACE|"
+echo "|        $BONJOUR : $PSEUDO$ESPACE|"
 echo "|                                                    |"
 REPERTOIRE=$1
 NBESPACE=${#REPERTOIRE}
@@ -19,14 +44,14 @@ while [ $NBESPACE != "0" ]; do
 	ESPACE="$ESPACE "
 	NBESPACE=`expr $NBESPACE "-" "1"`
 done
-echo "| Répertoire par defaut :                            |"
+echo "| $MESSREP :                            |"
 echo "| $REPERTOIRE$ESPACE |"
-echo "| D) afficher l’espace disque occupé                 |"
-echo "| F) fixer le répertoire par défaut                  |"
-echo "| C) créer un répertoire                             |"
-echo "| E) Editer un texte                                 |"
-echo "| M) Envoyer un mail                                 |"
-echo "| S) Envoyer un fichier par mail                     |"
-echo "| Q) se déconnecter                                  |"
+echo "| D) $MESSD                 |"
+echo "| F) $MESSF                  |"
+echo "| C) $MESSC                             |"
+echo "| E) $MESSE                                 |"
+echo "| M) $MESSM                                 |"
+echo "| S) $MESSS                  |"
+echo "| Q) $MESSQ                                  |"
 echo "------------------------------------------------------"
-echo "  Votre choix (D,F,C,E,M,S,Q) ?"
+echo "  $MESSCHOICE"
